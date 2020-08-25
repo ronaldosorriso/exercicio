ff4b0d3db4a81ff4fb8ea16169d93d2891c813b5
Lucas Silveira <lukinhaxd010@gmail.com>
2020-08-24 22:14:37 -0300

    exe9 testado

---
+++ exe9.js
@@ -0,0 +1,9 @@
+function exe9(numero){
+    for(var i = numero; 1 >= 1; i--){
+        if (numero % i == 0){
+            soma = soma + i
+
+        }
+    }
+    console.log(`Soma dos divisores é ${soma}`)
+}
